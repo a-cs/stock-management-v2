c05e7395c13cbb768a21a679ac69269d18ae3726
a-cs <acarneirosousa@gmail.com>
2025-02-11 22:32:12 -0300

    updating index.html title

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,13 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-const iconLink = document.createElement('link')
-iconLink.rel = 'icon'
-iconLink.href = '/ifce.svg'
-document.head.appendChild(iconLink)
-
-document.title = 'Estoque LEM - IFCE'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
